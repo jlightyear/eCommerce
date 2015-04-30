452c7ed9cb33a1839d085fcc71c59b27affcbe88
jlightyear <joseluis.estrach@gmail.com>
2015-04-30 23:16:05 +0200

    Fix some code lines

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -17,7 +17,6 @@ class OmniauthCallbacksController < ApplicationController
       sign_in_and_redirect @usuario, event: :authentication
     else
       session[:omniauth_errors] = @usuario.errors.full_messages.to_sentence unless @usuario.save
-
       session[:omniauth_data] = data
 
       redirect_to new_usuario_registration_url
